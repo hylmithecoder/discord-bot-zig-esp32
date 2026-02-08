85fa4bcbfd412dd5613bf5fa3d4a1abd3614fb42
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2026-02-08 19:46:11 +0700

    Discord Bot on esp32 0.1

---
+++ main/app.zig
@@ -0,0 +1,179 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const idf = @import("esp_idf");
+const wifi = idf.wifi;
+const ver = idf.ver.Version;
+const ESP_LOG = idf.log.ESP_LOG;
+
+export fn app_main() callconv(.c) void {
+    // This allocator is safe to use as the backing allocator w/ arena allocator
+    // std.heap.raw_c_allocator
+
+    // custom allocators (based on raw_c_allocator)
+    // idf.heap.HeapCapsAllocator
+    // idf.heap.MultiHeapAllocator
+    // idf.heap.vPortAllocator
+
+    var heap = idf.heap.HeapCapsAllocator.init(.MALLOC_CAP_8BIT);
+    var arena = std.heap.ArenaAllocator.init(heap.allocator());
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    log.info("Hello, world from Zig!", .{});
+
+    log.info(
+        \\[Zig Info]
+        \\* Version: {s}
+        \\* Compiler Backend: {s}
+        \\
+    , .{
+        @as([]const u8, builtin.zig_version_string), // fix esp32p4(.xesppie) fmt-slice bug
+        @tagName(builtin.zig_backend),
+    });
+
+    ESP_LOG(allocator, tag,
+        \\[ESP-IDF Info]
+        \\* Version: {s}
+        \\
+    , .{ver.get().toString(allocator)});
+
+    ESP_LOG(
+        allocator,
+        tag,
+        \\[Memory Info]
+        \\* Total: {d}
+        \\* Free: {d}
+        \\* Minimum: {d}
+        \\
+    ,
+        .{
+            heap.totalSize(),
+            heap.freeSize(),
+            heap.minimumFreeSize(),
+        },
+    );
+
+    ESP_LOG(
+        allocator,
+        tag,
+        "Let's have a look at your shiny {s} - {s} system! :)\n\n",
+        .{
+            @tagName(builtin.cpu.arch),
+            builtin.cpu.model.name,
+        },
+    );
+
+    arraylist(allocator) catch |err| {
+        log.err("Error: {s}", .{@errorName(err)});
+    };
+
+    if (builtin.mode == .Debug)
+        heap.dump();
+
+    // FIXME: NOT BUILD on H2 ('builtin.cpu.model.name' not exists esp32h2)
+    wifi_init() catch |err| {
+        log.err("Error: {s}", .{@errorName(err)});
+    };
+
+    // FreeRTOS Tasks
+    if (idf.rtos.xTaskCreate(foo, "foo", 1024 * 3, null, 1, null) == 0) {
+        @panic("Error: Task foo not created!\n");
+    }
+    if (idf.rtos.xTaskCreate(bar, "bar", 1024 * 3, null, 2, null) == 0) {
+        @panic("Error: Task bar not created!\n");
+    }
+    if (idf.rtos.xTaskCreate(blinkclock, "blink", 1024 * 2, null, 5, null) == 0) {
+        @panic("Error: Task blinkclock not created!\n");
+    }
+}
+
+fn stringToArray(comptime size: usize, str: [:0]const u8) [size]u8 {
+    var arr: [size]u8 = undefined;
+    @memset(&arr, 0); // Zero-fill
+    const len = @min(str.len, size);
+    @memcpy(arr[0..len], str[0..len]);
+    return arr;
+}
+
+fn wifi_init() !void {
+    var conf: wifi.wifiConfig = .{
+        .sta = .{
+            .password = stringToArray(64, "pass"),
+            .ssid = stringToArray(32, "my_ssid"),
+        },
+    };
+    try wifi.init(&.{});
+    try wifi.setMode(.WIFI_MODE_STA);
+    try wifi.setConfig(.WIFI_IF_STA, &conf);
+    try wifi.start();
+    try wifi.connect();
+}
+
+// comptime function
+fn blinkLED(delay_ms: u32) !void {
+    try idf.gpio.Direction.set(
+        .GPIO_NUM_18,
+        .GPIO_MODE_OUTPUT,
+    );
+    while (true) {
+        log.info("LED: ON", .{});
+        try idf.gpio.Level.set(.GPIO_NUM_18, 1);
+
+        idf.rtos.vTaskDelay(delay_ms / idf.rtos.portTICK_PERIOD_MS);
+
+        log.info("LED: OFF", .{});
+        try idf.gpio.Level.set(.GPIO_NUM_18, 0);
+    }
+}
+
+fn arraylist(allocator: std.mem.Allocator) !void {
+    var arr: std.ArrayList(u32) = .empty;
+    defer arr.deinit(
+        allocator,
+    );
+
+    try arr.append(allocator, 10);
+    try arr.append(allocator, 20);
+    try arr.append(allocator, 30);
+
+    for (arr.items) |index| {
+        ESP_LOG(
+            allocator,
+            tag,
+            "Arr value: {}\n",
+            .{index},
+        );
+    }
+}
+// Task functions (must be exported to C ABI) - runtime functions
+export fn blinkclock(_: ?*anyopaque) void {
+    blinkLED(1000) catch |err|
+        @panic(@errorName(err));
+}
+
+export fn foo(_: ?*anyopaque) callconv(.c) void {
+    while (true) {
+        log.info("Demo_Task foo printing..", .{});
+        idf.rtos.vTaskDelay(2000 / idf.rtos.portTICK_PERIOD_MS);
+    }
+}
+export fn bar(_: ?*anyopaque) callconv(.c) void {
+    while (true) {
+        log.info("Demo_Task bar printing..", .{});
+        idf.rtos.vTaskDelay(1000 / idf.rtos.portTICK_PERIOD_MS);
+    }
+}
+
+// override the std panic function with idf.panic
+pub const panic = idf.esp_panic.panic;
+const log = std.log.scoped(.@"esp-idf");
+pub const std_options: std.Options = .{
+    .log_level = switch (builtin.mode) {
+        .Debug => .debug,
+        else => .info,
+    },
+    // Define logFn to override the std implementation
+    .logFn = idf.log.espLogFn,
+};
+
+const tag = "zig-example";
